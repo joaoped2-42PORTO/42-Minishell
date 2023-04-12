74aa6aac3793d710206d97b935fe1be70e9bf246
Hugo Fernando Oliveira Silva <huolivei@c1r3s7.42porto.com>
2023-04-12 15:59:07 +0100

    tests

--- src/test.c
+++ src/test.c
@@ -13,3 +13,16 @@ int main()
 
     return 0;
 }
+/*
+//Criar o prompt necessario para inicializar o Shell
+int main()
+{
+	char *str = readline("$");
+	rl_replace_line("%%", 1);
+	add_history(str);
+	rl_clear_history();
+	rl_on_new_line();
+	rl_redisplay();
+	printf("%s eliminada;\n", str);
+	free(str);
+}*/
