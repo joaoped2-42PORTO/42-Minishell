613d73277e1ec4346f2728ac5e4cd4d27281a2cc
joaoped2 <joaoped2@c1r7s1.42porto.com>
2023-04-14 11:40:22 +0100

    norm checked

--- test.c
+++ test.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaoped2 <joaoped2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 10:44:04 by joaoped2          #+#    #+#             */
+/*   Updated: 2023/04/14 10:45:32 by joaoped2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	main()
+#include "includes/minishell.h"
+
+int	main(void)
 {
-	char *str;
+	char	*str;
+
 	str = readline(str);
 	printf("%s\n", str);
 	free (str);
