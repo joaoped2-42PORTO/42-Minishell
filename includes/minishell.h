b1437d4b0a2eb1ffabb853aa7c743ffec73313e2
joaoped2 <joaoped2@c1r7s1.42porto.com>
2023-04-14 11:39:29 +0100

    added ft_strncmp

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaoped2 <joaoped2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 10:45:48 by joaoped2          #+#    #+#             */
+/*   Updated: 2023/04/14 11:03:53 by joaoped2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -19,5 +31,9 @@
 # include <termios.h>
 # include <curses.h>
 # include <term.h>
+
+//Utils Functions
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
