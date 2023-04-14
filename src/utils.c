7066b30274b3838ab4a5edf90a7ed44fd58e0fe9
joaoped2 <joaoped2@c1r7s1.42porto.com>
2023-04-14 11:40:03 +0100

    created utils

---
+++ src/utils.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaoped2 <joaoped2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 10:41:24 by joaoped2          #+#    #+#             */
+/*   Updated: 2023/04/14 11:03:34 by joaoped2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned char	*ptr1;
+	unsigned char	*ptr2;
+
+	ptr1 = (unsigned char *)s1;
+	ptr2 = (unsigned char *)s2;
+	while (n && *ptr1 && *ptr1 == *ptr2)
+	{
+		++ptr1;
+		++ptr2;
+		--n;
+	}
+	if (n)
+		return (*ptr1 - *ptr2);
+	else
+		return (0);
+}
