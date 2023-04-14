403e14e854f1f234700ffa945beeae4aab1fcad3
joaoped2 <joaoped2@c1r7s1.42porto.com>
2023-04-14 11:40:47 +0100

    added stuff that might be useful

--- src/test.c
+++ src/test.c
@@ -1,22 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaoped2 <joaoped2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 10:46:07 by joaoped2          #+#    #+#             */
+/*   Updated: 2023/04/14 11:37:31 by joaoped2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
-int main()
+int	main(void)
 {
-    char *input;
+	char	*input;
 
-    while ((input = readline("👾Phylothinkers👾> ")) != NULL) {
+	while (1)
+	{
+		input = readline("👾Phylothinkers👾> ");
 		if (input && *input)
 			add_history(input);
-		if (!strcmp(input, "history")) { system("history"); }
-		else if (!strcmp(input, "ifconfig")) { system("ifconfig"); }
-		else if (!strcmp(input, "clear")) { system("clear"); }
-        else
-			printf("%s\n", input);
-        free(input);
-    }
-
-    return 0;
+		if (!ft_strncmp(input, "clear", 5))
+			system("clear");
+		else
+			printf("command not found: %s\n", input);
+		free(input);
+	}
+	return (0);
 }
+/*
+	#include <stdlib.h>
+	int system(const char *command);
+
+	The system() library function uses fork(2) to create a child
+	process that executes the shell command specified in command
+	using execl(3) as follows:
+
+		execl("/bin/sh", "sh", "-c", command, (char *) NULL);
+*/
 /*
 //Criar o prompt necessario para inicializar o Shell
 int main()
