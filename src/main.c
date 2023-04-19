cb663bf74ba6d7ee82e545122d793d8da8dd40bb
joaoped2 <joaoped2@c1r12s18.42porto.com>
2023-04-19 17:04:27 +0100

    removed commands to give it a clear space only for main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: joaoped2 <joaoped2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 22:11:07 by huolivei          #+#    #+#             */
-/*   Updated: 2023/04/19 11:13:38 by joaoped2         ###   ########.fr       */
+/*   Updated: 2023/04/19 16:28:35 by joaoped2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,83 +15,6 @@
 // Neste momento ja se consegue alterar o directory onde nos encontramos,
 // so que ainda nao funciona o cd sem mais nenhum argumento. Pelo que li,
 // teremos que usar o env!
-
-void	do_cd(char *str)
-{
-	int	i;
-	int	j;
-	int	error;
-	char	*cd;
-
-	i = 3;
-	j = 0;
-	error = 0;
-	while (str[i])
-		i++;
-	cd = malloc(i + 1 * sizeof(char));
-	i = 3;
-	while (str[i])
-		cd[j++] = str[i++];
-	cd[j] = '\0';
-	//printf("%s\n", cd);
-	error = chdir(cd);
-	if (error != 0)
-		printf("Wrong directory!\n");
-	free (cd);
-}
-
-void	check_pwd(void)
-{
-	char	path[1000];
-	getcwd(path, sizeof(path));
-	if (path[0] == '\0')
-		printf("Something went wrong!\n");
-	else
-		printf("%s\n", path);
-}
-
-// Mostra todos os files no current path, already fixed, ja nao mostra ficheiros que comecem por "."
-void	check_files_in_path(void)
-{
-	/*
-	Function: DIR *opendir (const char *dirname)
-	The opendir function opens and returns a directory stream for reading the directory whose file name is dirname. The stream has type DIR *.
-	*/
-	DIR	*d;
-	/*
-		DIR: A type representing a directory stream.
-
-		It also defines the structure dirent which includes the following members:
-			ino_t  d_ino       file serial number
-			char   d_name[]    name of entry
-
-		The type ino_t is defined as described in <sys/types.h>.
-		The character array d_name is of unspecified size, but the number of bytes preceding the terminating null byte will not exceed {NAME_MAX}.
-		The following are declared as functions and may also be defined as macros. Function prototypes must be provided for use with an ISO C compiler.
-
-		int            closedir(DIR *);
-		DIR           *opendir(const char *);
-		struct dirent *readdir(DIR *);
-		int            readdir_r(DIR *, struct dirent *, struct dirent **);
-		void           rewinddir(DIR *);
-		void           seekdir(DIR *, long int);
-		long int       telldir(DIR *);
-	*/
-	struct dirent	*dir;
-	d = opendir(".");
-	if (d)
-	{
-		while ((dir = readdir(d)) != NULL)
-		{
-			if (!ft_strcmp(dir->d_name, "."))
-				continue;
-			else
-				printf("%s\n", dir->d_name);
-		}
-		closedir(d);
-	}
-}
-
 
 int	main(void)
 {
@@ -114,7 +37,7 @@ int	main(void)
 		else if (!ft_strncmp(input, "exit", 4))
 			return (0);
 		else if (!ft_strncmp(input, "ls", 2))
-			check_files_in_path();
+			check_files_in_path(input);
 		else
 			printf("command not found: %s\n", input);
 		free(input);
