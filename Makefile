64c0848fbee58816d586686abce55892cb57596f
joaoped2 <joaoped2@c1r12s18.42porto.com>
2023-04-20 16:30:59 +0100

    no more merges

--- Makefile
+++ Makefile
@@ -7,26 +7,38 @@ NAME	= minishell
 CC		= cc
 CFLAGS	= -Wall -Wextra -Werror
 
+HSHELL  = hugoshell
+JSHELL  = juanitoshell
 
 SRC		= src/main.c src/utils/utils.c src/commands/cd.c src/commands/pwd.c src/handler/handler.c src/signals/signals.c
+SRCHUGO = hugo/src/main.c hugo/src/utils/utils.c hugo/src/commands/cd.c hugo/src/commands/pwd.c hugo/src/handler/handler.c hugo/src/signals/signals.c
+SRCJUAN = juanito/src/main.c juanito/src/utils/utils.c juanito/src/commands/cd.c juanito/src/commands/pwd.c juanito/src/handler/handler.c juanito/src/signals/signals.c
 OBJ		= $(SRC:.c=.o)
+OBJHUGO = $(SRCHUGO:.c=.o)
+OBJUAN	= $(SRCJUAN:.c=.o)
 
 all: $(OBJ)
 		@echo "$(RED)[ .. ]Compiling Mandatory[ .. ]$(RESET)"
 		@$(CC) $(CFLAGS) $(SRC) -lreadline -o $(NAME)
 		@echo "$(GREEN)[ OK ]$(RESET)$(YELLOW)Mandatory Ready!$(RESET)$(GREEN)[ OK ]$(RESET)"
 
+hugo: $(OBJHUGO)
+		$(CC) $(CFLAGS) $(SRCHUGO) -lreadline -o $(HSHELL)
+
+juan: $(OBJUAN)
+		$(CC) $(CFLAGS) $(SRCJUAN) -lreadline -o $(JSHELL)
+
 src/%.o:	src/%.c
 			@$(CC) $(CFLAGS) -o $@ -c $<
 
 clean:
 	@echo "$(RED)[ .. ]Deleting objects[ .. ]$(RESET)"
-	@rm -rf $(OBJ)
+	@rm -rf $(OBJ) $(OBJHUGO) $(OBJUAN)
 	@echo "$(GREEN)[ OK ]Objects deleted[ OK ]$(RESET)"
 
 fclean: clean
 	@echo "$(RED)[ .. ]Deleting Programs[ .. ]$(RESET)"
-	@rm -rf $(NAME)
+	@rm -rf $(NAME) $(JSHELL) $(HSHELL)
 	@echo "$(GREEN)[ OK ]Programs deleted[ OK ]$(RESET)"
 
 re: fclean all
---
+++ juanito/includes/minishell.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaoped2 <joaoped2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 10:45:48 by joaoped2          #+#    #+#             */
+/*   Updated: 2023/04/20 14:46:08 by joaoped2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <sys/ioctl.h>
+# include <sys/syscall.h>
+# include <dirent.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <string.h>
+# include <errno.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+
+//Utils Functions
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strdup(char *str);
+
+//Commands
+void	check_files_in_path(char *str);
+void	do_cd(char *str);
+void	check_pwd(void);
+
+//Handler
+int		cmdhandler(char *str);
+
+//Signals
+
+void	config_signals(void);
+
+#endif
