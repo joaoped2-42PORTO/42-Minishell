59d8897960cbfceffddc2374066393486b8842d6
Hugo Fernando Oliveira Silva <huolivei@c1r3s7.42porto.com>
2023-05-11 10:43:20 +0100

    pcs freezing, testing errors of builtins

--- hugo/src/commands/pwd.c
+++ hugo/src/commands/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joaoped2 <joaoped2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: huolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 16:28:28 by joaoped2          #+#    #+#             */
-/*   Updated: 2023/04/19 16:30:15 by joaoped2         ###   ########.fr       */
+/*   Updated: 2023/05/11 10:42:42 by huolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	check_pwd(void)
 
 	getcwd(path, sizeof(path));
 	if (path[0] == '\0')
-		printf("Something went wrong!\n");
+		perror("");
 	else
 		printf("%s\n", path);
 }
