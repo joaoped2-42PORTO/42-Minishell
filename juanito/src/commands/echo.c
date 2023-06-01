78b42b2d6b1a6f30ccddc66592643bbd69c9ed7b
joaoped2 <nedponey@gmail.com>
2023-06-01 09:41:39 +0100

    No more leaks on echo in the tests i made, double quotes and single quotes are working fine aswell, the only thing that needs to be fixed is the strcmp on the dollar variables

--- juanito/src/commands/echo.c
+++ juanito/src/commands/echo.c
@@ -32,58 +32,67 @@ int	checkforspacesinstring(t_shell *args, int i)
 	return (j);
 }
 
-void	checksplitcontent(t_shell *args)
+void checksplitcontent(t_shell *args)
 {
-	int	i;
-	int	j;
-	int	x;
+    int i;
+    int j;
+    int x;
 
-	i = 0;
-	j = 1;
-	x = countvalues(args);
-	if (x == 4)
-	{
-		printf("\n");
-		return ;
-	}
-	else if (args->split[j][i] == '-' && args->split[j][i + 1] == 'n')
-	{
-		while (args->split[j][i] == '-')
-		{
-			if (args->split[j][i] == '-' && args->split[j][i + 1] == 'n')
-			{
-				i++;
-				while (args->split[j][i] == 'n')
-					i++;
-				if (args->split[j][i] == '\0')
-				{
-					j++;
-					x += i;
-					x += checkforspacesinstring(args, x);
-					i = 0;
-				}
-				else
-				{
-					printf("%s", checkbars(args, &x));
-					return ;
-				}
-			}
-			else
-			{
-				printf("%s", checkbars(args, &x));
-				return ;
-			}
-		}
-	}
-	else
-	{
-		i = countvalues(args);
-		printf("%s", checkbars(args, &i));
-		printf("\n");
-		return ;
-	}
-	printf("%s", checkbars(args, &x));
+    i = 0;
+    j = 1;
+    x = countvalues(args);
+    if (x == 4)
+    {
+        printf("\n");
+        return;
+    }
+    else if (args->split[j][i] == '-' && args->split[j][i + 1] == 'n')
+    {
+        while (args->split[j][i] == '-')
+        {
+            if (args->split[j][i] == '-' && args->split[j][i + 1] == 'n')
+            {
+                i++;
+                while (args->split[j][i] == 'n')
+                    i++;
+                if (args->split[j][i] == '\0')
+                {
+                    j++;
+                    x += i;
+                    x += checkforspacesinstring(args, x);
+                    i = 0;
+                }
+                else
+                {
+                    char *result = checkbars(args, &x);
+                    printf("%s", result);
+                    free(result);
+                    return;
+                }
+            }
+            else
+            {
+                char *result = checkbars(args, &x);
+                printf("%s", result);
+                free(result);
+                return;
+            }
+        }
+    }
+    else
+    {
+        i = countvalues(args);
+        char *result = checkbars(args, &i);
+        printf("%s", result);
+        free(result);
+        printf("\n");
+        return;
+    }
+    char *result = checkbars(args, &x);
+    printf("%s", result);
+    free(result);
 }
+
 
 void	do_echo(t_shell *args)
 {
--- juanito/src/handler/tokens.c
+++ juanito/src/handler/tokens.c
@@ -17,6 +17,7 @@ char *checkbars(t_shell *args, int *i)
     int x;
     int p;
     int k;
+    int t;
     char *str = NULL;
     char *ptr = NULL;
     char *ptr2 = NULL;
@@ -28,6 +29,7 @@ char *checkbars(t_shell *args, int *i)
 	x = *i;
 	p = 0;
 	k = 0;
+    t = 0;
     while (args->input[x])
     {
         if (valid_input(args) == 0)
@@ -36,12 +38,52 @@ char *checkbars(t_shell *args, int *i)
             break;
         }
 
-        if (args->input[x] == '"' || p % 2 != 0)
+        if ((args->input[x] == '"' || p % 2 != 0) && t % 2 == 0)
         {
             if (args->input[x] == '"')
             {
                 x++;
                 p++;
+            }
+            else if (args->input[x] == '$' && args->input[x + 1] != ' ' && args->input[x + 1] != '\0')
+            {
+                x++;
+                str = (char *)malloc((ft_strlen(args->input) + 1) * sizeof(char));
+                while (args->input[x] != ' ' && args->input[x] != '\0' && args->input[x] != '$' && args->input[x] != '"')
+                    str[k++] = args->input[x++];
+                str[k] = '\0';
+                ptr2 = print_env_var(args, str);
+                if (ptr2)
+                {
+                    tmp = malloc(ft_strlen(res) + ft_strlen(ptr2) + 1);
+                    strcpy(tmp, res);
+                    strcat(tmp, ptr2);
+                    free(res);
+                    res = tmp;
+                    free(ptr2);
+                }
+                free(str);
+                k = 0;
+            }
+            else
+            {
+                ptr = (char *)malloc(2 * sizeof(char));
+                ptr[0] = args->input[x++];
+                ptr[1] = '\0';
+                tmp = malloc(ft_strlen(res) + ft_strlen(ptr) + 1);
+                strcpy(tmp, res);
+                strcat(tmp, ptr);
+                free(res);
+                res = tmp;
+                free(ptr);
+            }
+        }
+        else if ((args->input[x] == '\'' || t % 2 != 0) && p % 2 == 0)
+        {
+            if (args->input[x] == '\'')
+            {
+                x++;
+                t++;
             }
             else if (args->input[x] == '$' && args->input[x + 1] != ' ' && args->input[x + 1] != '\0')
             {
