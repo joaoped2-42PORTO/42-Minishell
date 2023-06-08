a511d7b7eda331cb74f330dbfb0d9ae21b8a8210
joaoped2 <joaoped2@c1r9s9.42porto.com>
2023-06-08 11:43:12 +0100

    more fixes to echo with flag

--- juanito/includes/minishell.h
+++ juanito/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: joaoped2 <joaoped2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 10:45:48 by joaoped2          #+#    #+#             */
-/*   Updated: 2023/06/07 14:57:50 by joaoped2         ###   ########.fr       */
+/*   Updated: 2023/06/08 11:34:35 by joaoped2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,5 +102,6 @@ void		do_echo(t_shell *args);
 int			checkforspacesinstring(t_shell *args, int i);
 int			isdoublequote(t_shell *args);
 void		print_processed_result(t_shell *args, char *res);
+int			checkisalpha(t_shell *args, int *i);
 
 #endif
